0ea3cff43906ed6a82a5c6b38a5309f8e7224878
Bryan Culbertson <bryan.culbertson@gmail.com>
2023-12-30 22:27:04 +0000

    Update isort

--- src/actransit_rt/functions/main.py
+++ src/actransit_rt/functions/main.py
@@ -1,7 +1,16 @@
 import flask
 import functions_framework
+from cloudevents.http.event import CloudEvent
 
 
 @functions_framework.http
 def hello(request: flask.Request) -> flask.typing.ResponseReturnValue:
-    return "Hello world!"
+    data = request.json or {}
+    name = data.get("name", "world")
+
+    return f"Hello {name}!"
+
+
+@functions_framework.cloud_event
+def ping(cloud_event: CloudEvent) -> None:
+    print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
