7bd074140708226fbf080dc04bc106ac2f038e82
Bryan Culbertson <bryan.culbertson@gmail.com>
2023-12-30 21:02:13 -0800

    Basic reading and writing (#9)

---
+++ src/actransit_rt/functions/archive.py
@@ -0,0 +1,6 @@
+"""Utilities for storing feeds
+
+/actransit/realtime/tripupdates/2024/02/15/1703994731.tripupdates.pb.gz
+/actransit/realtime/alerts/2024/02/15/1703994731.alerts.pb.gz
+/actransit/realtime/vehicles/2024/02/15/1703994731.vehicles.pb.gz
+"""
--- src/actransit_rt/functions/main.py
+++ src/actransit_rt/functions/main.py
@@ -14,3 +14,14 @@ def hello(request: flask.Request) -> flask.typing.ResponseReturnValue:
 @functions_framework.cloud_event
 def ping(cloud_event: CloudEvent) -> None:
     print(f"Received event with ID: {cloud_event['id']} and data {cloud_event.data}")
+
+
+@functions_framework.http
+def snapshot(request: flask.Request) -> flask.typing.ResponseReturnValue:
+    data = request.json or {}
+    is_dryrun = data.get("dryrun", "false".lower()) == "true"
+
+    if is_dryrun:
+        return "Dry run"
+
+    return "Real run"
